fe8c14768999722f40ab40f0443028d30f0943b5
Sergey <sorokin.sns@yandex.ru>
2021-09-23 19:57:53 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -6,41 +6,56 @@
 
 #include <stdlib.h>
 
-//#pragma warning(disable: 4996)
+//#pragma warning(disable: 4996) */
 
 #define TRUE 1
 #define FALSE 0
 #define ERROR - 1
 
 int main() {
-  char buffer;
-  unsigned int value = 0;
-  int count = 0;
-  int sign = FALSE;
-  while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') {
-    if ((buffer == '-') && (sign == FALSE)) {
-      sign = TRUE;
-    } else {
+	char buffer;
+	unsigned int value = 0;
+	int count = 0;
+	int sign = FALSE;
+	if ((((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0' && buffer == '-') && sign == FALSE) {
+		sign = TRUE;
+		while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') {
 
-      if ((buffer == '1') || (buffer == '0')) {
-        value <<= 1;
-        value += buffer - '0';
-        count++;
-      } else {
-        printf("Error.\n");
-        exit(ERROR);
-      }
-    }
-  }
-  if (count > sizeof(int) * CHAR_BIT) {
-    printf("Error oversize.\n");
-    exit(ERROR);
-  }
-  if (sign == TRUE) {
-    printf("-");
-  }
-  printf("%u\n", value);
-  return 0;
+			if ((buffer == '1') || (buffer == '0')) {
+				value <<= 1;
+				value += buffer - '0';
+				count++;
+			}
+			else {
+				printf("Error.\n");
+				exit(ERROR);
+			}
+		}
+	}
+	else {
+		while ((buffer != EOF) && buffer != '\n' && buffer != '\0') {
+
+			if ((buffer == '1') || (buffer == '0')) {
+				value <<= 1;
+				value += buffer - '0';
+				count++;
+			}
+			else {
+				printf("Error.\n");
+				exit(ERROR);
+			}
+			buffer = getchar();
+		}
+	}
+	if (count > sizeof(int) * CHAR_BIT) {
+		printf("Error oversize.\n");
+		exit(ERROR);
+	}
+	if (sign == TRUE) {
+		printf("-");
+	}
+	printf("%u\n", value);
+	return 0;
 }
 //00000000000000000000000000000000
-//11111111111111111111111111111111
+//11111111111111111111111111111111   
