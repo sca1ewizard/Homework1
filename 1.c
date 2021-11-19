abb685dd8e4ca7cee3dd2f90b7ab10375d310850
sca1ewizard <90906573+sca1ewizard@users.noreply.github.com>
2021-11-19 17:51:32 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,61 +1,49 @@
-#include <stdint.h>
+#include <stdint.h> 
 
 #include <stdio.h>
 
 #include <limits.h>
 
-#include <stdlib.h>
-
-//#pragma warning(disable: 4996)
-
-#define TRUE 1
-#define FALSE 0
-#define ERROR - 1
-
+#include <stdlib.h>                                    //подключаем все нужные нам библиотеки
+                                                       //
+//#pragma warning(disable: 4996)                       //заглушаем предупреждения компиляторав (в начале программа использовала scanf и компилятор сильно ругался) 
+                                                       
+#define TRUE 1                                         //используется для определения наличия у числа знака "+" или "-" 
+#define FALSE 0                                        //используется для определения наличия у числа знака "+" или "-"
+#define ERROR - 1                                      //объявляем все макроподстановки
+                                                       
 int main() {
-	char buffer;
-	unsigned int value = 0;
-	int count = 0;
-	int sign = FALSE;
-	if ((((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0' && buffer == '-') && sign == FALSE) {
-		sign = TRUE;
-		while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') {
+    char buffer;                                       //непосредственно введённый с клавиатуры символ
+    unsigned int value = 0;                            //значение результата по модулю
+    int count = 0;                                     //подсчет символов результата (используется для определения переполнения) 
+    int sign = FALSE;
+    while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') {  
+                                                       //если введенный сивол не конец файла и не сивол перехода на новую строку и не сивол конца строки) 
+        if ((buffer == '-') && (sign == FALSE)) {      //определяем знак
+            sign = TRUE;                               
+        }
+        else {
 
-			if ((buffer == '1') || (buffer == '0')) {
-				value <<= 1;
-				value += buffer - '0';
-				count++;
-			}
-			else {
-				printf("Error.\n");
-				exit(ERROR);
-			}
-		}
-	}
-	else {
-		while ((buffer != EOF) && buffer != '\n' && buffer != '\0') {
-
-			if ((buffer == '1') || (buffer == '0')) {
-				value <<= 1;
-				value += buffer - '0';
-				count++;
-			}
-			else {
-				printf("Error.\n");
-				exit(ERROR);
-			}
-			buffer = getchar();
-		}
-	}
-	if (count > sizeof(int) * CHAR_BIT) {
-		printf("Error oversize.\n");
-		exit(ERROR);
-	}
-	if (sign == TRUE) {
-		printf("-");
-	}
-	printf("%u\n", value);
-	return 0;
+            if ((buffer == '1') || (buffer == '0')) {  //в двоичной записи числа содержаться только числа 1 и 0, так что для них...
+                value <<= 1;                           //сдвигаем побитово value на 1 бит влево
+                value += buffer - '0';                 //и прибавляем к нему значение буфера
+                count++;                               //инкриминируем счётчик символов
+            }
+            else {                                     //иначе, если символ не 1 или 0 или -, значит символ введён неправильно и надо выдать ошибку 
+                printf("Error.\n");                    //печатаем ошибку
+                exit(ERROR);                           //завершаем программу с кодом ошибки -1
+            }
+        }
+    }                                                  
+    if (count > sizeof(int) * CHAR_BIT) {              //Проверка не переполнение
+        printf("Error oversize.\n");                   //если количество символов больше чем максимально количество бит для данной архитектуры памяти, то выводим ошибку
+        exit(ERROR);
+    }
+    if (sign == TRUE) {                                //если есть знак "-", то печатаем его
+        printf("-");
+    }
+    printf("%u\n", value);                             //выводим результат
+    return 0;
 }
-//00000000000000000000000000000000
+//00000000000000000000000000000000                     //Просто проверял по этим числам перепонение, чтобы каждый раз не вбивать 32 единицы
 //11111111111111111111111111111111
